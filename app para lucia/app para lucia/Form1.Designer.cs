b2dc8818b6a1c7aee801aaa2152680ecf3248ad0
adrian peromingo <jguillen.adrianperomingo@gmail.com>
2016-05-20 17:03:23 +0200

    UPDATESS

---
+++ app para lucia/app para lucia/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace app_para_lucia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bFoto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bFoto
+            // 
+            this.bFoto.Location = new System.Drawing.Point(379, 78);
+            this.bFoto.Name = "bFoto";
+            this.bFoto.Size = new System.Drawing.Size(134, 115);
+            this.bFoto.TabIndex = 0;
+            this.bFoto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 262);
+            this.Controls.Add(this.bFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bFoto;
+    }
+}
+
